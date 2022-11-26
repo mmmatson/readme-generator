466ba2c72c048ae8ce0ce6cf135be53eb9ce5213
mmm <margaret.m.matson@gmail.com>
2022-11-26 17:19:29 -0600

    changed variable name to reflect changes in index.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,15 @@
 //Function returns a license badge based on which license is passed in
-function renderLicenseBadge(answer) { 
-if (answer.license === 'MIT License') {
+function renderLicenseBadge(answers) { 
+if (answers.license === 'MIT License') {
   return `![MIT License Badge](https://img.shields.io/badge/license-MIT-blue)`
 }
-else if (answer.license === 'Apache License 2.0') {
+else if (answers.license === 'Apache License 2.0') {
   return `![Apache 2.0 License Badge](https://img.shields.io/badge/license-Apache%202.0-blue)`
 }
-else if (answer.license === 'GPL License (aka GNU General Public License v3.0)') {
+else if (answers.license === 'GPL License (aka GNU General Public License v3.0)') {
   return '![GNU GPLv3 License Badge](https://img.shields.io/badge/license-GNU%20GPLv3-blue)'
 }
-else if (answer.license === 'The Unlicense') {
+else if (answers.license === 'The Unlicense') {
   return `![The Unlicense License Badge](https://img.shields.io/badge/license-The%20Unlicense-blue)`
 }
 // If there is no license, return an empty string
@@ -19,17 +19,17 @@ else {
 }
 
 //Function returns the license section of README
-function renderLicenseSection(answer) {
-  if (answer.license === 'MIT License') {
+function renderLicenseSection(answers) {
+  if (answers.license === 'MIT License') {
     return `This application is covered under the MIT License.`
   }
-  else if (answer.license === 'Apache License 2.0') {
+  else if (answers.license === 'Apache License 2.0') {
     return `This application is covered under the Apache License Version 2.0.`
   }
-  else if (answer.license === 'GPL License (aka GNU General Public License v3.0)') {
+  else if (answers.license === 'GPL License (aka GNU General Public License v3.0)') {
     return 'This application is covered under the GNU GENERAL PUBLIC LICENSE Version 3'
   }
-  else if (answer.license === 'The Unlicense') {
+  else if (answers.license === 'The Unlicense') {
     return `This application is covered under The Unlicense.`
   }
   // If there is no license, return no license string
@@ -39,12 +39,13 @@ function renderLicenseSection(answer) {
 }
 
 //Generate markdown for README
-function generateMarkdown(answer) {
-  return `${renderLicenseBadge(answer)}
-  # ${answer.title}
+function generateMarkdown(answers) {
+  return `${renderLicenseBadge(answers)}
+
+  # ${answers.title}
 
   ## Description
-  ${answer.description}
+  ${answers.description}
 
   ## Table of Contents
   - [Installation](#installation)
@@ -55,24 +56,24 @@ function generateMarkdown(answer) {
   - [License](#license)
 
   ## Installation
-  ${answer.installation}
+  ${answers.installation}
 
   ## Usage
-  ${answer.usage}
+  ${answers.usage}
 
   ## Contributing
-  ${answer.contributing}
+  ${answers.contributing}
 
   ## Tests
-  ${answer.tests}
+  ${answers.tests}
 
   ## Questions
   You may send questions about this project to me through:
-  - GitHub: [${answer.github}](https://github.com/${answer.github})
-  - Email: [${answer.email}](${answer.email})
+  - GitHub: [${answers.github}](https://github.com/${answers.github})
+  - Email: [${answers.email}](${answers.email})
 
   ## License
-  ${renderLicenseSection(answer)}`
+  ${renderLicenseSection(answers)}`
 }
 
 module.exports = generateMarkdown;
